4a019966305bc069a676e93a6a2b63e0a4cac189
Krang84 <patelassad@gmail.com>
2026-01-13 13:18:01 +0100

    Create prog1.java

---
+++ prog1.java
@@ -0,0 +1,10 @@
+public class prog1 {
+    public static void main(String[] args) {
+        System.out.println(4/3);
+        System.out.println((float)4/3);
+      // 1.3333334
+        System.out.println((double)4/3);
+   // 1.3333333333333333
+
+    }
+}
