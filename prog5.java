a9d54f22a5af181f269db294e7665fcf40d6d2ff
Krang84 <patelassad@gmail.com>
2026-01-21 09:16:01 +0100

    Create prog5.java

---
+++ prog5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class prog5 {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int n = reader.nextInt();
+        System.out.println(n);
+    }
+    
+}
