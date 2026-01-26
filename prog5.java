dfa849c1e03b7fb312e05cef890f87b74d9f9cc8
Krang84 <patelassad@gmail.com>
2026-01-26 09:02:43 +0100

    Update prog5.java

--- prog5.java
+++ prog5.java
@@ -2,9 +2,29 @@ import java.util.Scanner;
 public class prog5 {
 
     public static void main(String[] args) {
+
+        // input  notes
+        // output moyenne
         Scanner reader = new Scanner(System.in);
-        int n = reader.nextInt();
-        System.out.println(n);
+
+        // notes
+
+        System.out.println("Entrez note");
+
+        int n1 = reader.nextInt();
+        System.out.println("Entrez note");
+        int n2 = reader.nextInt();        
+        System.out.println("Entrez note");
+        int n3 = reader.nextInt();
+        System.out.println("Entrez note");
+        int n4 = reader.nextInt();
+
+       // moyenne
+
+       double moyenne = (n1+n2+n3+n4)/4.0;
+
+       System.out.println("La moyenne est " + moyenne);
+
     }
     
 }
